33fbea7223565669db624324515a4be75821b941
Hernán Valencia <frjr17@criptext.com>
2023-10-31 23:45:17 -0500

    fix: changing banner to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,38 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div
+          className="banner h-screen bg-cover bg-center"
+          style={{ backgroundImage: "url(/images/banner-photo.png)" }}
+        >
+          <div className="container mx-auto bg-daintree-950/80 h-screen px-10">
+            <div className="top-bar py-5 border-b border-slate-600 flex justify-between items-center">
+              <Image
+                src={"/svg/logo-gold.svg"}
+                width={200}
+                height={200}
+                alt={"Legality logo"}
+              />
+              <div className="ctas flex gap-8 items-center">
+                <div className="cart-container flex items-center gap-3">
+                  <Image
+                    src={"/images/CartIcon.png"}
+                    width={30}
+                    height={30}
+                    alt={"CartIcon"}
+                  />
+                  <span>Tu Carrito (0)</span>
+                </div>
+                <button className="py-6 px-8 bg-white/10">
+                  Agenda una Cita
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div
-      className="banner h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url(/images/banner-photo.png)" }}
-    >
-      <div className="container mx-auto bg-daintree-950/80 h-screen px-10">
-        <div className="top-bar py-5 border-b border-slate-600 flex justify-between items-center">
-          <Image
-            src={"/svg/logo-gold.svg"}
-            width={200}
-            height={200}
-            alt={"Legality logo"}
-          />
-          <div className="ctas flex gap-8 items-center">
-            <div className="cart-container flex items-center gap-3">
-              <Image
-                src={"/images/CartIcon.png"}
-                width={30}
-                height={30}
-                alt={"CartIcon"}
-              />
-              <span>Tu Carrito (0)</span>
-            </div>
-            <button className="py-6 px-8 bg-white/10">Agenda una Cita</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <></>;
 }
