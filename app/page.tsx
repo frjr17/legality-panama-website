1834a946594e7eace9a911ae34620458803c336e
Hernán Valencia <frjr17@criptext.com>
2023-10-31 20:05:15 -0500

    feat: setting top bar and its static files

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1 className="text-5xl">Legality Website</h1>;
+  return (
+    <div
+      className="banner h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url(/images/banner-photo.png)" }}
+    >
+      <div className="container mx-auto bg-daintree-950/80 h-screen px-10">
+        <div className="top-bar py-5 border-b border-slate-600 flex justify-between items-center">
+          <Image
+            src={"/svg/logo-gold.svg"}
+            width={150}
+            height={150}
+            alt={"Legality logo"}
+          />
+          <div className="ctas flex gap-5 items-center">
+            <div className="cart-container">
+              <div className="cart-icon"></div>
+              <span>Tu Carrito (0)</span>
+            </div>
+            <button className="py-4 px-7 bg-white/10">Agenda una Cita</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
