dc1634b42c790eb358d6b7906f8b9a65b12eb8b5
Hernán Valencia <frjr17@criptext.com>
2023-11-04 10:24:15 -0500

    feat: adding mobile topbar component

--- app/components/topbar/index.tsx
+++ app/components/topbar/index.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
+import Navbar from "../navbar";
 
-export default function Topbar() {
+function MobileTopbar() {
   return (
-    <nav className="top-bar py-5 border-b border-slate-600 flex justify-between items-center">
-      <Image
-        src={"/svg/logo-gold.svg"}
-        width={200}
-        height={200}
-        alt={"Legality logo"}
-      />
-      <div className="ctas flex gap-8 items-center">
+    <nav className="top-bar md:hidden py-5 border-b border-slate-600 flex justify-between items-center">
+      <div className=""></div>
+      <div className="">
+        <Image
+          src={"/svg/logo-gold.svg"}
+          width={200}
+          height={200}
+          alt={"Legality logo"}
+        />
+      </div>
+      <div className="">
+        <Image src={"/svg/bars.svg"} width={40} height={40} alt={"Menu Icon"} />
+      </div>
+      <div className="fixed  bg-white w-screen h-screen top-0 left-0">
+        <Navbar />
+      </div>
+      {/* <div className="ctas flex gap-8 items-center">
         <div className="cart-container flex items-center gap-3">
           <Image
             src={"/images/cart-icon.png"}
@@ -20,7 +30,35 @@ export default function Topbar() {
           <span>Tu Carrito (0)</span>
         </div>
         <button className="py-5 px-8 bg-white/10">Agenda una Cita</button>
-      </div>
+      </div> */}
     </nav>
+  );
+}
+
+export default function Topbar() {
+  return (
+    <>
+      <nav className="top-bar hidden py-5 border-b border-slate-600 md:flex justify-between items-center">
+        <Image
+          src={"/svg/logo-gold.svg"}
+          width={200}
+          height={200}
+          alt={"Legality logo"}
+        />
+        <div className="ctas flex gap-8 items-center">
+          <div className="cart-container flex items-center gap-3">
+            <Image
+              src={"/images/cart-icon.png"}
+              width={30}
+              height={30}
+              alt={"CartIcon"}
+            />
+            <span>Tu Carrito (0)</span>
+          </div>
+          <button className="py-5 px-8 bg-white/10">Agenda una Cita</button>
+        </div>
+      </nav>
+      <MobileTopbar />
+    </>
   );
 }
