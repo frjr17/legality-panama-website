17215ffe1d3bbfe2cafa19f1f1441337f31f0ce6
Hernán Valencia <frjr17@criptext.com>
2023-11-08 18:57:07 -0500

    feat: creating responsive nav

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -4,8 +4,35 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+interface ICTAProps {
+  href: string;
+  icon: string;
+  alt: string;
+  label: string;
+  text: string;
+  onClick?: () => void;
+}
+
+export function CTA(props: ICTAProps) {
+  return (
+    <Link
+      onClick={props.onClick ? props.onClick : undefined}
+      href={props.href}
+      className="cta flex gap-2 items-center"
+    >
+      <div className="cta-icon">
+        <Image src={props.icon} width={40} height={40} alt={props.alt} />
+      </div>
+      <div className="content">
+        <div className="label text-yellow-500 text-sm">{props.label}</div>
+        <div className="text text-md">{props.text}</div>
+      </div>
+    </Link>
+  );
+}
+
 export default function Navbar() {
-  const [showMobileNav, setShowMobileNav] = useState(false);
+  const [showMobileNav, setShowMobileNav] = useState(true);
   return (
     <div className="md md:container md:mx-auto sticky top-0 left-0 navbar text-white">
       <nav className="top hidden md:flex justify-between border-b border-slate-600 py-6">
@@ -34,46 +61,27 @@ export default function Navbar() {
         <div className="navs flex w-1/2 justify-between">
           {navbarNavs.map((nav) => {
             return (
-              <Link className="nav" href={nav.href}>
+              <Link key={nav.name} className="nav" href={nav.href}>
                 {nav.name}
               </Link>
             );
           })}
         </div>
         <div className="ctas flex gap-10 justify-between">
-          <Link href="tel:+50762266183" className="cta flex gap-2 items-center">
-            <div className="cta-icon">
-              <Image
-                src="/images/phone-icon.png"
-                width={40}
-                height={40}
-                alt="Phone Icon"
-              />
-            </div>
-            <div className="content">
-              <div className="label text-yellow-500 text-sm">Llámanos Al:</div>
-              <div className="text text-md">+507 6226-6183</div>
-            </div>
-          </Link>
-          <Link
+          <CTA
+            href="tel:+50762266183"
+            icon="/images/phone-icon.png"
+            alt="Phone Icon"
+            label="Llámanos Al:"
+            text="+507 6226-6183"
+          />
+          <CTA
             href="mailto:legality.pa@gmail.com"
-            className="cta flex gap-2 items-center"
-          >
-            <div className="cta-icon">
-              <Image
-                src="/images/email-icon.png"
-                width={40}
-                height={40}
-                alt="Email Icon"
-              />
-            </div>
-            <div className="content">
-              <div className="label text-yellow-500 text-sm">
-                Escríbenos al Correo:
-              </div>
-              <div className="text text-md">legality.pa@gmail.com</div>
-            </div>
-          </Link>
+            icon="/images/email-icon.png"
+            alt="Email Icon"
+            label="Escríbenos al Correo:"
+            text="legality.pa@gmail.com"
+          />
         </div>
       </nav>
       <MobileNavbar
@@ -114,14 +122,54 @@ function MobileNavbar(props: {
           props.showMobileNav ? "block" : "hidden"
         } bg-daintree-950 fixed top-0 left-0`}
       >
-        <Image
-          onClick={props.setShowMobileNav}
-          src="/svg/close-icon.svg"
-          className="absolute right-5 top-10"
-          alt={"navbar"}
-          width={30}
-          height={30}
-        />
+        <header className="relative  py-6 px-5 flex">
+          <div className="w-[40px]"></div>
+          <Image
+            className="mx-auto"
+            src="/svg/logo-gold.svg"
+            alt={"Legality Logo"}
+            width={175}
+            height={175}
+          />
+
+          <Image
+            onClick={props.setShowMobileNav}
+            src="/svg/close-icon.svg"
+            alt={"navbar"}
+            width={30}
+            height={30}
+          />
+        </header>
+        <div className="navs flex flex-col px-5 border-collapse">
+          {navbarNavs.map((nav) => (
+            <Link
+              className="border-y border-slate-600 py-6"
+              onClick={props.setShowMobileNav}
+              key={nav.name}
+              href={nav.href}
+            >
+              {nav.name}
+            </Link>
+          ))}
+        </div>
+        <div className="flex flex-col gap-5 mt-6 px-5">
+          <CTA
+            onClick={props.setShowMobileNav}
+            href="tel:+50762266183"
+            icon="/images/phone-icon.png"
+            alt="Phone Icon"
+            label="Llámanos Al:"
+            text="+507 6226-6183"
+          />
+          <CTA
+            onClick={props.setShowMobileNav}
+            href="mailto:legality.pa@gmail.com"
+            icon="/images/email-icon.png"
+            alt="Email Icon"
+            label="Escríbenos al Correo:"
+            text="legality.pa@gmail.com"
+          />
+        </div>
       </nav>
     </div>
   );
